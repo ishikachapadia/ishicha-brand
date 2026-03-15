eed38a8e563bae90636b4131fb81195ec0176b0f
Ishika <ishikachapadia@gmail.com>
2026-03-15 15:23:18 -0400

    Added social icons to the footer

--- wp-content/themes/arcline/functions.php
+++ wp-content/themes/arcline/functions.php
@@ -2,6 +2,7 @@
 
 function arcline_enqueue_styles() {
     wp_enqueue_style('arcline-style', get_stylesheet_uri(), array(), '1.0.0');
+    wp_enqueue_style('bootstrap-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css');
 }
 
 add_action('wp_enqueue_scripts', 'arcline_enqueue_styles');

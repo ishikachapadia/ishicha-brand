977c798faaddcb8db2e12a48a715d1b737656c62
Ishika <ishikachapadia@gmail.com>
2026-03-15 21:08:19 -0400

    edited functions.php to add a woocommerce css file

--- wp-content/themes/arcline/functions.php
+++ wp-content/themes/arcline/functions.php
@@ -2,6 +2,11 @@
 
 function arcline_enqueue_styles() {
     wp_enqueue_style('arcline-style', get_stylesheet_uri(), array(), '1.0.0');
+    
+    if ( class_exists( 'WooCommerce' ) ) {
+        wp_enqueue_style('arcline-woo-custom', get_template_directory_uri() . '/css/woocommerce-custom.css', array(), '1.0.0');
+    }
+
     wp_enqueue_style('bootstrap-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css');
 }
 
